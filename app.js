08b1aba1eb686c649af40d32a128bef4f334da73
lauriv <lauriv@greeny.cs.tlu.ee>
2016-02-10 10:50:05 +0200

    added singelton pattern

---
+++ app.js
@@ -0,0 +1,21 @@
+(function(){
+  "use strict";
+
+  var Moosipurk = function(){
+
+    //SINGELTON PATTERN
+    if(Moosipurk.instance){
+      return Moosipurk.instance;
+    }
+    Moosipurk.instance = this; //this viitab moosipurgile
+
+    console.log('moosipurgi sees');
+  };
+
+  window.onload = function(){
+    var app = new Moosipurk();
+  };
+
+
+
+})();
